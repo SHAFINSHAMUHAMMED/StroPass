3402ea35198bbf33d6f4c7a6df7d82d23395dc7a
SHAFINSHAMUHAMMED <122475859+SHAFINSHAMUHAMMED@users.noreply.github.com>
2023-10-05 15:29:59 +0530

    this is first commit

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const manifestForPlugin = {
+  registerType: 'autoUpdate',
+  devOptions: {
+    enabled: true
+  },
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  manifest: {
+    short_name: 'StroPass',
+    name: 'StroPass',
+    icons: [
+      {
+        src: '/passStrong.png',
+        type: 'image/png',
+        sizes: '512x512',
+      },
+      {
+        src: '/passStrong.png',
+        type: 'image/png',
+        sizes: '192x192',
+      },
+    ],
+    id: '/?source=pwa',
+    start_url: '/?source=pwa',
+    background_color: 'red',
+    display: 'standalone',
+    scope: '/',
+    splashScreens: [
+      {
+        src: '/passStrong.png',
+        sizes: '1024x1024',
+      },
+    ],
+  },
+};
+
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
